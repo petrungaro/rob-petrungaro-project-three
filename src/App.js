1383beb2e697f6b3a3017edceef1acd43d257062
Rob Petrungaro <rob.petrungaro@gmail.com>
2022-06-17 10:32:19 -0400

    added functionality to create a new API call when the base currency is changed - code still needs cleanup

--- src/App.js
+++ src/App.js
@@ -16,12 +16,20 @@ function App() {
     // user input in the text field
     // user selection for the currency 
 const [ coinData, setCoinData ] = useState([]);
-// TEST - pulling the input change up from Form
+// TEST - pulling the input change up from Form - update: works so far!
 const [searchValue, setSearchValue]  = useState('');
+// TEST - same as above, but for currency selector
+const [currencyValue, setCurrencyValue] = useState('cad');
 
+// Passed to Form via props
 const inputChange = ( newValue ) => {
-  console.log(newValue);
   setSearchValue(newValue);
+}
+
+// Passed to Form via props
+const currencyChange = (newValue) => {
+  console.log(newValue);
+  setCurrencyValue(newValue);
 }
 
 
@@ -40,28 +48,34 @@ const inputChange = ( newValue ) => {
       url: 'https://api.coingecko.com/api/v3/coins/markets',
       method: 'get',
       params: {
-        vs_currency: 'cad', // ! this will be a variable later on
+        vs_currency: currencyValue, // ! this will be a variable later on
         order: 'market_cap_desc',
         per_page: 50,
         price_change_percentage: '24h,7d',
       }
-    }, [])
+    })
     .then((response) => {
 
+      console.log('new API Call!')
       // some error handling? if statusText='OK'?... else?
 
       // setFunction(response.data (array of 100 objects))
       setCoinData(response.data);
     })
 
-  }, [])
+  }, [currencyValue])
 
 
   return (
     <div className="App">
       <h1>testing testing 123</h1>
 
-      <Form changeFunc={inputChange} testVal={searchValue}/>
+      <Form 
+        searchChange={inputChange} 
+        inputVal={searchValue}
+        currChange={currencyChange}
+        currVal={currencyValue}
+        />
       {/* The CoinList function was running faster than the api call was returned */}
       {/* QUESTION FOR INSTRUCTORS - how do I make sure the component doesn't load until the API call is complete and successful? could I use useEffect inside a component that depends on a state change in it's parent? */}
       { coinData.length > 0 
--- src/Form.js
+++ src/Form.js
@@ -6,26 +6,37 @@ const Form = (props) => {
 
     // ! Removed searchValue state from Form and moved to App, so that it can pass the value down to CoinList for filtering.
     // const [searchValue, setSearchValue]  = useState('');
-    const [selectValue, setSelectValue] = useState('cad');
+    // const [selectValue, setSelectValue] = useState('cad');
     
     // ! Removed for same reason as above, don't delete yet
     // const handleInputChange = (e) => {
     //     setSearchValue(e.target.value)
     // }
 
-    const handleSelectChange = (e) => {
-        setSelectValue(e.target.value)
-    }
+    // const handleSelectChange = (e) => {
+    //     setSelectValue(e.target.value)
+    // }
 
 
     // I'll need an onSubmit that will send whatever the user typed plus the currency value up to App to set those values 
 
     return (
         <form action="">
-            <input onChange={(e)=> {
-                props.changeFunc(e.target.value)
-            }} value={props.searchValue} type="text"/>
-            <select onChange={handleSelectChange} value={selectValue} name="currency" id="currency">
+            <input 
+                onChange={(e)=> { 
+                    props.searchChange(e.target.value)
+                }} 
+                value={props.inputVal} 
+                type="text"
+            />
+            <select 
+                onChange={(e)=> {
+                    props.currChange(e.target.value);
+                }} 
+                value={props.currVal} 
+                name="currency" 
+                id="currency"
+            >
                 <option value="cad">CAD</option>
                 <option value="usd">USD</option>
             </select>
